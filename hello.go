3e71326ffc3626be17eba62f80dee5640d8b0dba
Rodrigo Lemos <rodrigoflemos@gmail.com>
2024-11-20 11:28:41 -0300

    feat: :sparkles: create variables and test short declaration

--- hello.go
+++ hello.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	nome := "Rodrigo" // Tipo da variavel inferido // O var pode ser substituido por :=
+	versao := 1.1     // Tipo da variavel inferido, o go cria como float64, // O var pode ser substituido por :=
+	fmt.Println("Este programa está na versão", versao)
+	fmt.Println("O tipo da variavel nome é ", reflect.TypeOf(nome))
+	fmt.Println("O tipo da variavel versao é ", reflect.TypeOf(versao))
 }
