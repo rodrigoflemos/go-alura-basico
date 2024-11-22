f318135c71198286fa537bbe8bbda5ebe11a295b
Rodrigo Lemos <rodrigoflemos@gmail.com>
2024-11-22 17:42:45 -0300

    feat: :sparkles: create site monitoring with file

--- hello.go
+++ hello.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -60,7 +63,7 @@ func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
 	fmt.Println()
 
-	sites := []string{"https://httpbin.org/status/200", "https://httpbin.org/status/204", "https://httpbin.org/status/400"}
+	sites := leSitesDoArquivo()
 
 	for i := 0; i < monitoramentos; i++ {
 		for i, site := range sites {
@@ -74,11 +77,39 @@ func iniciarMonitoramento() {
 }
 
 func testaSite(site string) {
-	resp, _ := http.Get(site)
+	resp, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
 
 	if resp.StatusCode == 200 {
 		fmt.Println("Site:", site, "foi carregado com sucesso!")
 	} else {
 		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
 	}
+}
+
+func leSitesDoArquivo() []string {
+
+	var sites []string
+	arquivo, err := os.Open("sites.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	leitor := bufio.NewReader(arquivo)
+
+	for {
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+
+		sites = append(sites, linha)
+		if err == io.EOF {
+			break
+		}
+	}
+	arquivo.Close()
+	return sites
 }
